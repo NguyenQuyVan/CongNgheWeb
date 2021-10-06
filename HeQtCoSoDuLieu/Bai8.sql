ce83bd4d0197e791fa6cccb2f63c8c0964a6c2e3
kougarui <91733172+kougarui@users.noreply.github.com>
2021-10-06 20:24:41 +0700

    update

---
+++ HeQtCoSoDuLieu/Bai8.sql
@@ -0,0 +1,30 @@
+﻿select *
+into Custom_noindex
+from KhachHang
+
+select *from 
+KhachHang
+insert into Custom_noindex values (N'003',N'Võ Huỳnh ',N'Hằng','0999999999',N'Hang@gmail.com')
+insert into Custom_noindex values (N'004',N'Võ Thị ',N'Nga','0786687787',N'Nga@gmail.com')
+select *from 
+Custom_noindex
+
+--tìm KH có mã 004
+select * from Custom_noindex
+where MaKH='004'
+--tạo 1 cluster 
+create clustered index index_clustered_maKH
+on dbo.custom_noindex(MaKH);
+--thực thi câu lệnh dạng cluster
+select * from Custom_noindex
+where MaKH='004'
+
+select *
+from KhachHang
+where Email ='an@gmail.com'
+
+--tạo non cluster cho email
+create	nonclustered index id_custom_email
+on KhachHang(email);
+
+--tạo non 
